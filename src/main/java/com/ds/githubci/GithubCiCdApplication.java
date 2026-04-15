6175a95914e01c7577ebf7754ff3d2a57ef09dbf
https://github.com/DurgaSaiKodali <kodalisai889@gmail.com>
2026-04-15 11:15:10 +0530

    git push -u origin master

--- src/main/java/com/ds/githubci/GithubCiCdApplication.java
+++ src/main/java/com/ds/githubci/GithubCiCdApplication.java
@@ -2,12 +2,21 @@ package com.ds.githubci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCiCdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubCiCdApplication.class, args);
-	}
+
+    @GetMapping("/wish")
+    public String wish(){
+        return "Good Morning GITHUB";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiCdApplication.class, args);
+    }
 
 }
